e7c4bd6d0389478f5445be54266802f76bbf3d62
niuwanpeng <niuwanpeng@baidu.com>
2017-05-31 19:33:30 +0800

    test build fail

---
+++ src/main/java/com.jlu.test/Test.java
@@ -0,0 +1,13 @@
+package com.jlu.test;
+
+/**
+ * Created by niuwanpeng on 17/5/6.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.ou.println("");
+
+    }
+
+}
